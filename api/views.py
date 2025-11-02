7eb2fe2fed5d1009f83828b86901caba68582474
Sumit Vishwakarma <sv0042595@gmail.com>
2025-11-02 14:26:43 +0530

    DjangoRest

--- api/views.py
+++ api/views.py
@@ -15,6 +15,16 @@ class RegisterView(APIView):
         data = request.data
         serializer = RegisterSerializer(data = data)
 
+        if not serializer.is_valid():
+            return Response({
+                "status": False,
+                "message": serializer.errors
+            })
+        
+        serializer.save()
+
+        return Response({"status" : True , "message" : "User Created"})
+
 
 
     # Model View Sets
